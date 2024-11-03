8b3f17042003c7bd9d112f152a4c9c634e33a2cc
ray <111891431+RayX81194@users.noreply.github.com>
2024-11-03 22:50:24 +0530

    Add files via upload

--- content.js
+++ content.js
@@ -17,3 +17,13 @@ const link = document.createElement('link');
 link.rel = 'stylesheet';
 link.href = chrome.runtime.getURL(`${theme}-theme.css`);
 document.head.appendChild(link);
+
+
+const style = document.createElement('style');
+style.textContent = `
+@import url('https://fonts.googleapis.com/css2?family=Readex+Pro:wght@160..700&display=swap');
+  * {
+        font-family: "Readex Pro", sans-serif !important;
+  }
+`;
+document.head.appendChild(style);
